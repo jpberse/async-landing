b8272f0d8b12e3f619b61c88e486f997de9de0fd
jpberse <jp.bersezio@gmail.com>
2023-10-31 20:24:02 -0300

    Updates

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCdAHaWcKdpbT5XkN2Er6BUQ&part=snippet%2Cid&order=date&maxResults=9';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '73a18c2bbfmsh756e58edcc2feabp1c24afjsn992ce56906ff',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+const content = null || document.getElementById('content');
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0,8).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Api no disponible');
+    }
+})();
